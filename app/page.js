4c5f54ba2dde60a91e81b0521ccf4ae3a80c913a
paulafiaschi <pau.fiaschi@gmail.com>
2023-09-20 09:45:53 +0200

    data fecthed

--- app/page.js
+++ app/page.js
@@ -1,8 +1,25 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import CategoryButton from "./components/CategoryButton";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://cocktails-240e.restdb.io/rest/las-otras", {
+    method: "GET",
+    headers: {
+      "cache-control": "no-cache",
+      "x-apikey": "613731bc43cedb6d1f97edad",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home(props) {
+  const data = await getData();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -10,7 +27,8 @@ export default function Home() {
         <label>Con qué te podemos ayudar?</label>
         <input type="search"></input>
       </div>
-
+      <h2>{data[0].Nombre}</h2>
+      {console.log(data[0])}
       <div>
         <CategoryButton category="Emergencias" />
         <CategoryButton category="Construyendo Comunidad" />
