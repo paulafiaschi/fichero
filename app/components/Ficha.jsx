5005a86fa1a0507aaefd534bb2f33751ec484787
paulafiaschi <pau.fiaschi@gmail.com>
2023-09-20 13:13:45 +0200

    closed file data

--- app/components/Ficha.jsx
+++ app/components/Ficha.jsx
@@ -1,9 +1,27 @@
 import styles from "../ficha.module.scss";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Ficha({ props }) {
-  return (
+  const [openFile, setOpenFile] = useState(false);
+
+  function handleFile() {
+    setOpenFile((old) => !old);
+    console.log(openFile);
+  }
+  return openFile === false ? (
+    <div className={styles.fichaClosed} onClick={() => handleFile()}>
+      <p className={styles.nombre}>
+        {props.Nombre} | {props.Tipo}
+      </p>
+      <p className={styles.tel}>📞 {props.Tel} </p>
+      <p>
+        🔗 <Link href={props.Web}>Página Web</Link>
+      </p>
+    </div>
+  ) : (
     <div className={styles.ficha}>
+      <p onClick={() => handleFile()}>X</p>
       <p>
         {props.Categoria} | {props.Tipo}
       </p>
@@ -13,12 +31,11 @@ export default function Ficha({ props }) {
         🗣️ {props.Contacto} | 📞 {props.Tel}
       </p>
       <p>
-        🔗 <Link href={props.Web}>{props.Web}</Link> | ✉️ {props.Email}
+        🔗 <Link href={props.Web}>Página Web</Link> | ✉️ {props.Email}
       </p>
       <p>📍 {props.Direccion}</p>
       <p>💬 {props.Idioma.join(", ")}</p>
       <p>💰 {props.Precio}</p>
-      <p>🗒️ {props.Descripcion}</p>
       <p>🗒️ {props.Descripcion}</p>
       <p>⁉️ {props.Tips}</p>
     </div>
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,7 @@ import CategoryButton from "./CategoryButton";
 import Ficha from "./Ficha";
 
 export default function Main(props) {
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("Todas");
   const [filteredList, setFilteredList] = useState(props.data);
 
   function filterList(f) {
@@ -16,11 +16,12 @@ export default function Main(props) {
   }
   return (
     <>
+      <h2>{filter}</h2>
       <div>
         <button
-          category="all"
+          category="Todas"
           onClick={() => {
-            setFilter("all");
+            setFilter("Todas");
           }}>
           Todas
         </button>
@@ -39,13 +40,16 @@ export default function Main(props) {
           Construyendo Comunidad
         </button>
       </div>
-      {filter === "all"
-        ? props.data.map((ficha) => {
-            return <Ficha props={ficha} key={ficha._id} />;
-          })
-        : filteredList.map((ficha) => {
-            return <Ficha props={ficha} key={ficha._id} />;
-          })}
+
+      <div className={styles.fichero}>
+        {filter === "Todas"
+          ? props.data.map((ficha) => {
+              return <Ficha props={ficha} key={ficha._id} />;
+            })
+          : filteredList.map((ficha) => {
+              return <Ficha props={ficha} key={ficha._id} />;
+            })}
+      </div>
     </>
   );
 }
