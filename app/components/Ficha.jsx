b671aabe09412377e12f0d1f094d28dee2e37eba
paulafiaschi <pau.fiaschi@gmail.com>
2023-09-20 14:02:08 +0200

    collapsible info

--- app/components/Ficha.jsx
+++ app/components/Ficha.jsx
@@ -1,43 +1,43 @@
 import styles from "../ficha.module.scss";
 import Link from "next/link";
-import { useState } from "react";
+
+import React from "react";
+import Collapsible from "react-collapsible";
 
 export default function Ficha({ props }) {
-  const [openFile, setOpenFile] = useState(false);
-
-  function handleFile() {
-    setOpenFile((old) => !old);
-    console.log(openFile);
-  }
-  return openFile === false ? (
-    <div className={styles.fichaClosed} onClick={() => handleFile()}>
-      <p className={styles.nombre}>
-        {props.Nombre} | {props.Tipo}
-      </p>
-      <p className={styles.tel}>📞 {props.Tel} </p>
-      <p>
-        🔗 <Link href={props.Web}>Página Web</Link>
-      </p>
+  let summary = (
+    <div className={styles.summary}>
+      <div className={styles.fichaClosed}>
+        <p className={styles.nombre}>
+          <span>{props.Nombre}</span> | <span>{props.Tipo}</span>
+        </p>
+        <p className={styles.tel}>
+          {props.Tel != ""
+            ? `📞 ${props.Tel}`
+            : props.Contacto != props.Tel
+            ? `🗣️ ${props.Contacto}`
+            : ""}
+          {console.log(props.Tel)}
+        </p>
+        <p>
+          🔗 <Link href={props.Web}>Página Web</Link> | ✉️ {props.Email}
+        </p>
+      </div>
+      <div className="arrow">^</div>
     </div>
-  ) : (
-    <div className={styles.ficha}>
-      <p onClick={() => handleFile()}>X</p>
-      <p>
-        {props.Categoria} | {props.Tipo}
-      </p>
-      <h2 className={styles.nombre}>{props.Nombre}</h2>
-
-      <p>
-        🗣️ {props.Contacto} | 📞 {props.Tel}
-      </p>
-      <p>
-        🔗 <Link href={props.Web}>Página Web</Link> | ✉️ {props.Email}
-      </p>
-      <p>📍 {props.Direccion}</p>
-      <p>💬 {props.Idioma.join(", ")}</p>
-      <p>💰 {props.Precio}</p>
-      <p>🗒️ {props.Descripcion}</p>
-      <p>⁉️ {props.Tips}</p>
-    </div>
+  );
+  return (
+    <>
+      <Collapsible trigger={summary} className={styles.header}>
+        <div className={styles.details}>
+          <p>
+            📍 {props.Direccion} | 💰 {props.Precio}
+          </p>
+          <p>💬 {props.Idioma.join(", ")}</p>
+          <p className={styles.descripcion}>🗒️ {props.Descripcion}</p>
+          <p>⁉️ {props.Tips}</p>
+        </div>
+      </Collapsible>
+    </>
   );
 }
